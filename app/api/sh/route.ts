9c779a303ee7574125389d516cf21701d3db3199
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2026-01-17 15:48:18 +1100

    Update route.ts

--- app/api/sh/route.ts
+++ app/api/sh/route.ts
@@ -1,6 +1,6 @@
 // app/api/paragraph/route.ts
 export function GET() {
-  return new Response("This is a single paragraph from a TS file", {
+  return new Response("asdasdasd", {
     status: 200,
     headers: { "Content-Type": "text/plain" },
   });

fbc18d299dfb3eacb455beea347834260950a122
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2026-03-11 00:56:21 +1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,11 @@ import { HeroUIProvider } from "@heroui/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Personal portfolio",
+  title: "Syntaxical's space",
+  description: "Main page is a portfolio, I'll add stuff other stuff later ig",
+  openGraph: {
+    images: ["https://images-ext-1.discordapp.net/external/SrxfW4yeu0AI5EKiBc0q6LvAb2tgTBfhAajEC3CQzNI/https/media.tenor.com/qbtP-__kJC0AAAPo/hypnosis-dog.mp4"], // relative to public folder
+  },
 };
 
 export default function RootLayout({
